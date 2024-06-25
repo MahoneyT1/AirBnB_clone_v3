f84cfe9d5e2e125cf4ebe804ad9b01b1a8b015ad
MahoneyT1 <ifeanyitech15@gmail.com>
2024-06-25 03:54:53 +0100

    added the stats api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,12 @@ if status is 200
 from api.v1.views import app_views
 from flask import jsonify
 from flask import request, make_response
+from models import storage
+from models import user, city, place, amenity, review, state
 
 
-@app_views.route("/status", strict_slashes=False ,methods=["GET"])
+@app_views.route("/status", strict_slashes=False,
+                 methods=["GET"])
 def status_of():
     """ status route """
 
@@ -16,5 +19,18 @@ def status_of():
     response = make_response(jsonify({"status": status}))
     response.headers['content-type'] = "application/json"
 
+    return response
 
+@app_views.route("/api/v1/stats", strict_slashes=False,
+                 methods=['GET'])
+def stats_check():
+    """ populates the statistics """
+    all_obj = {"User": storage.count(user.User),
+               "Place": storage.count(place.Place),
+               "State": storage.count(state.State),
+               "Review": storage.count(review.Review),
+               "Amenity": storage.count(amenity.Amenity),
+               "City": storage.count(city.City)
+               }
+    response = make_response(jsonify(all_obj))
     return response
