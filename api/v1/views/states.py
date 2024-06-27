b2f2d3c145451c20eb0d092c62271d00a91d7799
MahoneyT1 <ifeanyitech15@gmail.com>
2024-06-27 09:14:59 +0100

    bug fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,16 +8,37 @@ from models import storage
 from flask import jsonify, make_response, abort
 from models.state import State
 
-@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+@app_views.route("/states", methods=['GET'], strict_slashes=False)
 def state_get(state_id):
     """Retrieves the list of all State objects
     GET /api/v1/states
     """
+    
+    # create a list to extract list of all states in
+    list_of_states = []
+    # get the copy of States from storage
+    all_data = storage.all(State)
+
+    for key, value in all_data.items():
+        list_of_states.append({key:value.to_dict()})
+
+    # construct a response
+    response = make_response(jsonify(list_of_states), 200)
+    return response
+
+@app_views.route("/states/<state>", methods=['GET'], strict_slashes=False)
+def get_main_state(state_id):
+    """Gets a particular state by id
+    """
+    main_state = storage.get(cls=State, id=state_id)
+
+    if main_state is not None:
+        response = make_response(jsonify(main_state), 200)
+        return response
+    else:
+        return "not found"
 
 
-
-
- 
 
 # @app_views.route("/states/<state_id>",
 #                             strict_slashes=False,
