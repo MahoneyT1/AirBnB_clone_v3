47f5263bbcc7f2498eef81c4d6edba6642fd2070
MahoneyT1 <ifeanyitech15@gmail.com>
2024-08-07 09:04:14 +0100

    updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,7 +32,7 @@ class DBStorage:
         """Instantiate a DBStorage object"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_HOST = '100.26.53.35' #getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
